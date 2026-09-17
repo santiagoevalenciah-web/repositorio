47505db71c9fb57f9831b7b012883205211fe87c
santiagoevalenciah-web <santiagoe.valenciah@uqvirtual.edu.co>
2026-09-16 20:24:26 -0500

    Descripción del cambio

---
+++ src/main/java/Github.java
@@ -0,0 +1,18 @@
+public class Github {
+    public static void main(String[] args) {
+
+        String nombre = "Thiago";
+        String carrera = "Ingeniería de Sistemas";
+        int semestre = 10;
+
+        mostrarInformacion(nombre, carrera, semestre);
+    }
+
+    public static void mostrarInformacion(String nombre, String carrera, int semestre) {
+
+        System.out.println("=== INFORMACIÓN DEL ESTUDIANTE ===");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Semestre: " + semestre);
+    }
+}
